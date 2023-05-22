38dc9dfc60256e234d1ec34bc814c631e323bf0d
Supzero-0 <mat.lecanu@gmail.com>
2023-05-22 10:49:09 +0200

    Add DELETE function

--- app.js
+++ app.js
@@ -23,6 +23,8 @@ app.post("/api/movies", validateMovie, movieHandlers.postMovie);
 
 app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
 
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+
 const userHandlers = require("./userHandlers");
 
 app.get("/api/users", userHandlers.getUsers);
@@ -31,6 +33,8 @@ app.get("/api/users/:id", userHandlers.getUserById);
 app.post("/api/users", validateUser, userHandlers.postUser);
 
 app.put("/api/users/:id", validateUser, userHandlers.updateUser);
+
+app.delete("/api/users/:id", userHandlers.deleteUser);
 
 app.listen(port, (err) => {
   if (err) {
